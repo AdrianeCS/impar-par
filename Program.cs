81c310f0d3fc08c6f1bc4c3768db33a5047a26c1
Adriane <adrianedacosta21@gmail.com>
2022-07-20 01:09:24 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Tabela impar ou par");
+
+Console.WriteLine("obs: azul indica impar e vermelho para par");
+
+int n1;
+
+Console.Write("digite o número...:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 %2==0)
+{
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("par");
+}
+
+else
+{Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("impar");}
